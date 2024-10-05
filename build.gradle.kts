1dd7e764c79101b9aff125ffc50e92c0afd3457e
Urs Stotz <ursstotz@gmail.com>
2024-10-05 19:32:24 +0200

    Add JMH plugin and dependencies to build script
    Integrated the JMH plugin and required dependencies to enable benchmarking. Updated the build script to include JMH core and annotation processor dependencies for implementation and testing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("me.champeau.jmh") version "0.6.8"
 }
 
 group = "ch.typedef"
@@ -10,9 +11,17 @@ repositories {
 }
 
 dependencies {
+    implementation("org.openjdk.jmh:jmh-core:1.37")
+    annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.37")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-
+    // https://mvnrepository.com/artifact/org.openjdk.jmh/jmh-generator-annprocess
+    implementation("org.openjdk.jmh:jmh-generator-annprocess:1.37")
+    // https://mvnrepository.com/artifact/com.github.albfernandez/juniversalchardet
+    implementation("com.github.albfernandez:juniversalchardet:2.5.0")
+    // https://mvnrepository.com/artifact/org.openjdk.jmh/jmh-core
+    testImplementation("org.openjdk.jmh:jmh-core:1.37")
+    testImplementation("org.openjdk.jmh:jmh-generator-annprocess:1.37")
     testImplementation(kotlin("test"))
 }
 
